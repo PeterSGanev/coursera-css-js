4c2957aa8bbc150abb1011de34de7f9eaf6fd401
PeterSGanev <pganev@gmail.com>
2018-09-11 10:23:05 -0500

    Add files via upload

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,7 +83,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  buildAndShowHomeHTML, true); // Explicitely setting the flag to get JSON from server processed into an object literal
+  buildAndShowHomeHTML(categories), true); // Explicitely setting the flag to get JSON from server processed into an object literal
 });
 // *** finish **
 
@@ -115,6 +115,7 @@ function buildAndShowHomeHTML (categories) {
       // it into the home html snippet.
       //
       // var homeHtmlToInsertIntoMainPage = ....
+	  
 		 var homeHtmlToInsertIntoMainPage = $dc.loadMenuItems(chosenCategoryShortName);
 
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
